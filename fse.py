f7112f963e658c084682616e259788dcb916ca7f
Young Ma <yma114@ucsc.edu>
2023-11-14 10:52:34 -0500

    fun init

--- fse.py
+++ fse.py
@@ -6,3 +6,22 @@
 # Boston University
 # EC551, Fall 2023
 ########################################
+
+import json
+import fpga_adt as fpga
+    
+# analyze input equations and determine the order they are routed
+# most complex equations are routed first
+def analyze_eq():
+    pass
+
+# LUT routing with no connection constraints
+# and can be mapped to any LUT from any inputs
+def routing_complete():
+    pass
+
+# LUT routing with connection constraints
+def routing_constrained():
+    pass
+
+
